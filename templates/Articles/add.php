ef5e230ce6eead7d211b6ce71c247065f25bff84
Skuyer <angelpatatas099@gmail.com>
2026-05-21 10:36:54 -0600

    cambios en el archivo add.php

--- templates/Articles/add.php
+++ templates/Articles/add.php
@@ -1,9 +1,18 @@
-<h1>Añadir Artículo</h1>
-<?php
-    echo $this->Form->create($articles);
-    // Cambia 'title' y 'body' por los nombres reales de tus columnas en la base de datos
-    echo $this->Form->control('Articles');
-    echo $this->Form->control('creación', ['rows' => '3']);
-    echo $this->Form->button(__('Guardar Artículo'));
-    echo $this->Form->end();
-?>
+<div class="articles form content">
+    <?= $this->Form->create($article) ?>
+    <fieldset>
+        <legend><?= __('Añadir Artículo') ?></legend>
+        <?php
+            // El primer parámetro DEBE ser el nombre exacto de la columna en tu BD ('title')
+            echo $this->Form->control('title', ['label' => 'Título del Artículo']);
+            
+            // El primer parámetro DEBE ser el nombre exacto de la columna en tu BD ('body')
+            echo $this->Form->control('body', ['rows' => '5', 'label' => 'Contenido']);
+            
+            // El primer parámetro DEBE ser el nombre exacto de la columna en tu BD ('published')
+            echo $this->Form->control('published', ['label' => '¿Publicar inmediatamente?']);
+        ?>
+    </fieldset>
+    <?= $this->Form->button(__('Guardar Artículo')) ?>
+    <?= $this->Form->end() ?>
+</div>
