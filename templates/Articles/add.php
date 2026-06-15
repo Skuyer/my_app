8a3bb58c00b56119678e7cad275018b8a4648b25
Skuyer <angelpatatas099@gmail.com>
2026-06-15 12:22:22 -0600

    se editaron add.php y edit.php

--- templates/Articles/add.php
+++ templates/Articles/add.php
@@ -3,15 +3,16 @@
     <fieldset>
         <legend><?= __('Añadir Artículo') ?></legend>
         <?php
-            // El primer parámetro DEBE ser el nombre exacto de la columna en tu BD ('title')
             echo $this->Form->control('title', ['label' => 'Título del Artículo']);
-            
-            // El primer parámetro DEBE ser el nombre exacto de la columna en tu BD ('body')
             echo $this->Form->control('body', ['rows' => '5', 'label' => 'Contenido']);
-            
-            // El primer parámetro DEBE ser el nombre exacto de la columna en tu BD ('published')
             echo $this->Form->control('published', ['label' => '¿Publicar inmediatamente?']);
-            echo $this->Form->control('tags._ids', ['options' => $tags]);
+            
+            // CONFIGURADO: Forzado a renderizar una lista de selección múltiple con altura fija
+            echo $this->Form->control('tags._ids', [
+                'options' => $tags, 
+                'label' => 'Tags',
+                'style' => 'height: 100px;'
+            ]);
         ?>
     </fieldset>
     <?= $this->Form->button(__('Guardar Artículo')) ?>
--- templates/Articles/edit.php
+++ templates/Articles/edit.php
@@ -3,7 +3,14 @@
     echo $this->Form->create($article);
     echo $this->Form->control('title', ['label' => 'Título']);
     echo $this->Form->control('body', ['rows' => '3', 'label' => 'Contenido']);
+    
+    // MOVIDO: Ahora está dentro del formulario y formateado como una lista de selección múltiple
+    echo $this->Form->control('tags._ids', [
+        'options' => $tags, 
+        'label' => 'Tags',
+        'style' => 'height: 100px;' // Le da una altura cómoda para ver los elementos
+    ]);
+
     echo $this->Form->button(__('Guardar artículo'));
     echo $this->Form->end();
-    echo $this->Form->control('tags._ids', ['options' => $tags]);
 ?>
