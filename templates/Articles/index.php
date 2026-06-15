559698d7f3a56f38bfc109c9916180f8b84c6c09
Skuyer <angelpatatas099@gmail.com>
2026-06-15 12:12:52 -0600

    correciones en index.php

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -6,20 +6,21 @@
     <tr>
         <th>Título</th>
         <th>Creado</th>
-        <th>Acciones</th> </tr>
+        <th>Acciones</th> 
+    </tr>
 
     <?php foreach ($articles as $article): ?>
     <tr>
         <td>
-            <?= $this->Html->link($article->title, ['action' => 'view', $article->slug]) ?>
+            <?= $this->Html->link($article->title, ['action' => 'view', $article->id]) ?>
         </td>
         <td>
             <?= $article->created->format(DATE_RFC850) ?>
         </td>
         <td>
-            <?= $this->Html->link('Editar', ['action' => 'edit', $article->slug]) ?>
+            <?= $this->Html->link('Editar', ['action' => 'edit', $article->id]) ?>
             | 
-            <?= $this->Form->postLink('Eliminar', ['action' => 'delete', $article->slug], ['confirm' => '¿Estás seguro?']) ?>
+            <?= $this->Form->postLink('Eliminar', ['action' => 'delete', $article->id], ['confirm' => '¿Estás seguro?']) ?>
         </td>
     </tr>
     <?php endforeach; ?>
